4c229a33860c7a33edd39a5a8a4f1074cd00ccbc
zlog7918 <83124927+zlog7918@users.noreply.github.com>
2022-10-31 13:08:42 +0100

    Added is_connected

--- index.js
+++ index.js
@@ -6,32 +6,38 @@ const tmp = require('tmp')
 const x = require('throw-if-missing')
 
 module.exports = ({ exe = 'C:/Program Files (x86)/Cisco/Cisco AnyConnect Secure Mobility Client/vpncli.exe',
-                    server = x`server`, username = x`username`, password = x`password` }) => {
+					server = x`server`, username = x`username`, password = x`password`, acceptCertificate = `acceptCertificate`, group = `group` }) => {
 
-    this.connect = () =>
-        new Promise((resolve, reject) =>
-            tmp.file({ prefix: 'vpn-', postfix: '.txt' }, (err, path, fd, cleanup) => {
-                if (err) reject(err)
-                else writeFile(path, `${username}\n${password}\n`, err => {
-                    if (err) reject(err)
-                    else exec(`"${exe}" connect ${server} -s < ${path}`, (err, stdout) => {
-                        cleanup()
-                        if (err) reject(err)
-                        else if (!stdout.trim().endsWith('state: Connected')) reject(new Error(stdout))
-                        else resolve()
-                    })
-                })
-            })
-        )
-
-    this.disconnect = () =>
-        new Promise((resolve, reject) =>
-            exec(`"${exe}" disconnect`, (err, stdout) => {
-                if (err) reject(err)
-                else if (!stdout.trim().endsWith('state: Disconnected')) reject(new Error(stdout))
-                else resolve()
-            })
-        )
-
-    return this
+	return {
+		connect: ()=>new Promise((resolve, reject) =>
+			tmp.file({ prefix: 'vpn-', postfix: '.txt' }, (err, path, fd, cleanup) => {
+				acceptCertificate = acceptCertificate ? acceptCertificate+'\n':''
+				group = group ? group+'\n':''
+				if(err) reject(err)
+				else writeFile(path, `${acceptCertificate}${group}${username}\n${password}\n`, err => {
+					if(err) reject(err)
+					else exec(`"${exe}" connect ${server} -s < ${path}`, (err, stdout) => {
+						cleanup()
+						if(err) reject(err)
+						else if(!stdout.trim().endsWith('state: Connected')) reject(new Error(stdout))
+						else resolve()
+					})
+				})
+			})
+		),
+		disconnect: ()=>new Promise((resolve, reject) =>
+			exec(`"${exe}" disconnect`, (err, stdout) => {
+				if(err) reject(err)
+				else if(!stdout.trim().endsWith('state: Disconnected')) reject(new Error(stdout))
+				else resolve()
+			})
+		),
+		is_connected=()=>new Promise((resolve, reject)=>
+			exec(`"${exe}" state`, (err, stdout)=>{
+				if(err) reject(err)
+				else if(!stdout.trim().endsWith('state: Connected')) resolve(false)
+				else resolve(true)
+			})
+		)
+	}
 }
